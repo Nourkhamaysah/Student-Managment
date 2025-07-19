529bcfab267dbc040479b30d07377dde423607ab
Nourkhamaysah <noorsakh33@gmail.com>
2025-07-19 15:37:56 +0300

    STudentApp

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Student-Management-Static/',
+  base: '/Student-Management-Static/', // اسم المستودع
 });
